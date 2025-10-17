7134798d7d8122146a81a87fd805f22ffb3074eb
Khalil El Kaaki <khalil.el.kaaki@gmail.com>
2025-10-18 00:04:58 +0300

    Update main.m

--- main.m
+++ main.m
@@ -14,21 +14,21 @@ SIGNALDURATION = 2;
 CARRIERFREQUENCY = 50;
 
 % sampling_analysis(SIGNALDURATION, CARRIERFREQUENCY);
-% fourier_analysis(SIGNALDURATION, CARRIERFREQUENCY);
+fourier_analysis(SIGNALDURATION, CARRIERFREQUENCY);
 % quantization_analysis(SIGNALDURATION, CARRIERFREQUENCY);
 
-[t, xt, f_max] = exampleSpeechWave(SIGNALDURATION, CARRIERFREQUENCY);
-f_Nyquist = 2*f_max;
-% ----------------------------------
-[t_sample_nqyuist, x_sample_nyquist] = sample(t, xt, f_Nyquist);
-% ----------------------------------
-M = 64;
-tgtMSE = 0.01;
-[xq_u, MSEu] = uniformQuan(M, t_sample_nqyuist, x_sample_nyquist, false);
-[thr_n, lvl_n, xq_lm, MSElm] = lloydMax(t_sample_nqyuist, x_sample_nyquist, M, tgtMSE);
-% ----------------------------------
-coding_analysis(xq_u);
-coding_analysis(xq_lm);
-% ----------------------------------
-block_coding_analysis(xq_u , length(unique(xq_u)), [1 2 3 4], true);
-block_coding_analysis(xq_lm , length(unique(xq_lm)), [1 2 3 4], true);
+% [t, xt, f_max] = exampleSpeechWave(SIGNALDURATION, CARRIERFREQUENCY);
+% f_Nyquist = 2*f_max;
+% % ----------------------------------
+% [t_sample_nqyuist, x_sample_nyquist] = sample(t, xt, f_Nyquist);
+% % ----------------------------------
+% M = 64;
+% tgtMSE = 0.01;
+% [xq_u, MSEu] = uniformQuan(M, t_sample_nqyuist, x_sample_nyquist, false);
+% [thr_n, lvl_n, xq_lm, MSElm] = lloydMax(t_sample_nqyuist, x_sample_nyquist, M, tgtMSE);
+% % ----------------------------------
+% coding_analysis(xq_u);
+% coding_analysis(xq_lm);
+% % ----------------------------------
+% block_coding_analysis(xq_u , length(unique(xq_u)), [1 2 3 4], true);
+% block_coding_analysis(xq_lm , length(unique(xq_lm)), [1 2 3 4], true);
